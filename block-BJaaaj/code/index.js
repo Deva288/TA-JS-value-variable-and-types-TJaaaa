cb8fb2687404e4687d1d8cf78c3c43f6d9843b2e
Devanand kumar <deva.zebu288@gmail.com>
2022-11-22 14:35:55 +0530

    refactor

--- block-BJaaaj/code/index.js
+++ block-BJaaaj/code/index.js
@@ -6,18 +6,30 @@ let lastName = "Stark";
 // Do the following:
 
 // Alert the value of the firstName.
+alert("firstName");
 // Alert the value of the lastName.
+alert("lastName");
 // Assign a new value of `"John"` to firstName.
+firstName = "John"
 // Assign a new value of `"Snow"` to lastName.
+lastName = "Snow"
 // Alert the new value of `firstName` and `lastName` by concatenating them
+alert("firsName + lastName");
 // Create a new varaible using `const` named `fullName` and assign the fullname using firstName and lastName seperated by space.
-
+// const fullName = firstName + "" + lastName
+ const fullName = `${firstName} ${lastName}`;   
 // Declare three variables `name,` `newUser` and `isAdmin`.
+let name, newUser, isAdmin;
 // Assign your name as the value of `name`.
+Name = "Devanand"
 // Assign the value of `name` to `newUser`.
+newUser = Name
 // Set the value of `isAdmin` to `true`.
+isAdmin = "true";
 // Alert the values of all the three variables.
-
+alert(Name);
+alert(newUser);
+alert(isAdmin);
 // Assignment and re-assignment
 
 var wiseMan = "Tyrion Lannister";
@@ -39,3 +51,6 @@ let amount = 4280;
 // Declare another variable `addedAmount`. It's value should be 32 more than the value of amount.
 // Declare a variable `multipleAmount`. It's value should be 7 times the value of amount.
 // Declare a variable `dividedAmount`. It should store the resultant of amount divided by 57.
+
+
+
